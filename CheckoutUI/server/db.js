6ed47d1bd812ee64f205792c33d521e47d74704b
Amal Prasad <amalprasad354@gmail.com>
2023-02-28 15:31:52 +0530

    completd

--- CheckoutUI/server/db.js
+++ CheckoutUI/server/db.js
@@ -1,2 +1,23 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://adminUser:suVVOf6Vv6UNZqns@cluster0.6kubs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://adminUser:suVVOf6Vv6UNZqns@cluster0.6kubs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myProject';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
